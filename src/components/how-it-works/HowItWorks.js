aabc4a4d3b7503fcb491a62cf934c40ee241072e
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-10-06 12:36:24 +0100

    add how it works section

--- src/components/how-it-works/HowItWorks.js
+++ src/components/how-it-works/HowItWorks.js
@@ -12,31 +12,33 @@ const HowItWorks = () => {
         center='Save & invest cryptocurrency with ease'
         bottom='Create an account with these easy steps'
       />
-      <Container className='grid gap-4 mt-16 grid-cols-card'>
-        <HowCard
-          title='Sign Up'
-          description='Create your account entering your name and email account'
-        >
-          <Circle progress={25} />
-        </HowCard>
-        <HowCard
-          title='Log In'
-          description='Log into your dashboard using your email and password'
-        >
-          <Circle progress={50} />
-        </HowCard>
-        <HowCard
-          title='Fund Wallet'
-          description='Send cryptocurrency to the wallet automatically created for you'
-        >
-          <Circle progress={75} />
-        </HowCard>
-        <HowCard
-          title='Start Saving'
-          description='Select a saving plan and timeframe to save your funds'
-        >
-          <Circle progress={100} />
-        </HowCard>
+      <Container>
+        <section className='grid grid-cols-1 gap-4 mt-16 md:grid-cols-2'>
+          <HowCard
+            title='Sign Up'
+            description='Create your account entering your name and email account'
+          >
+            <Circle progress={25} />
+          </HowCard>
+          <HowCard
+            title='Log In'
+            description='Log into your dashboard using your email and password'
+          >
+            <Circle progress={50} />
+          </HowCard>
+          <HowCard
+            title='Fund Wallet'
+            description='Send cryptocurrency to the wallet automatically created for you'
+          >
+            <Circle progress={75} />
+          </HowCard>
+          <HowCard
+            title='Start Saving'
+            description='Select a saving plan and timeframe to save your funds'
+          >
+            <Circle progress={100} />
+          </HowCard>
+        </section>
       </Container>
     </section>
   );
