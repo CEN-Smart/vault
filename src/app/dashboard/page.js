d1b95b46d52630c30abac0caa65892b59ae0e200
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-10-08 11:41:17 +0100

    update dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import DashboardHeading from './../../components/dashboard/Heading';
 
+export const metadata = {
+  title: 'Dashboard | Vault',
+  description: 'Start saving and investing with ease',
+};
 const DashboardHome = () => {
   return (
     <>
